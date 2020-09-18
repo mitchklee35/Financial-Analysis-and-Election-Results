4dc3d0e273d6943a5a7982b2a16aadd9cde26116
Mitch Klee <mithcklee35@gmail.com>
2020-09-18 15:24:01 -0500

    final commit

--- PyPoll/pypoll2.py
+++ PyPoll/pypoll2.py
@@ -7,6 +7,7 @@
 import os
 import csv
 
+# File path to CSV
 Main_file = os.path.join("..","PyPoll","election_data.csv")
 
 
@@ -24,17 +25,17 @@ with open(Main_file) as csvfile:
     Candidates= []
 
     
-    
+    # For loop that counts the amount of voters
     for row in csv_reader:
         VoterID.append(row[0])
         Candidates = row[2]
         total_votes = (len(VoterID))
             
-        
+        # Appends list fi candidate is not in the table
         if (Candidates) not in (Candidate_name):
             Candidate_name.append(Candidates)
             Candidate_votes[Candidates] = 0
-        
+        # Adds one vote to the candidates total
         Candidate_votes[Candidates] += 1
         
     
@@ -46,12 +47,13 @@ with open(Main_file) as csvfile:
 # In[5]:
 
 
-
+# Prints the results for total 
 print("Election Results", )
 print("-------------------------------------------------------------")
 print("Total Votes:", (len(VoterID)))
 print("-------------------------------------------------------------")
 
+# Loop through candidates to create table and print the results. 
 for Candidate in Candidate_name:
     print(str(Candidate) + " " + str(round(Candidate_votes[Candidate]/total_votes*100))
           + "%" + " " + "(" + str(Candidate_votes[Candidate]) + ")")
